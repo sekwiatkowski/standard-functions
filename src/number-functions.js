1908c56a10c052783bdff7913d025cc7968635c3
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-08-22 05:47:56 +0200

    number-functions.js: added add and multiply

--- src/number-functions.js
+++ src/number-functions.js
@@ -2,6 +2,17 @@ function toString(input) {
     return input.toString()
 }
 
+function add(x) {
+    return y => x + y
+}
+
+
+function multiply(x) {
+    return y => x * y
+}
+
 module.exports = {
-    toString
+    toString,
+    add,
+    multiply
 }
