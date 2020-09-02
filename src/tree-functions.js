7d35506fd53b325ba08af83ca09d9513ea01fbb9
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-02 17:54:40 +0200

    tree-functions: renamed foldTree and concatTree to foldLeaves and concatLeaves

--- src/tree-functions.js
+++ src/tree-functions.js
@@ -1,17 +1,19 @@
 const {concat} = require('./string-or-array-functions')
 const {fold} = require('./array-functions')
 
-function foldTree(operation) {
+function foldLeaves(operation) {
     return empty => getChildren => isLeaf => onLeaf => node =>
         fold
-            ((acc, child) => operation(acc) (isLeaf(child) ? onLeaf(child) : concatTree(isLeaf)(onLeaf)(child)))
+            ((acc, child) => operation
+                (acc)
+                (isLeaf(child) ? onLeaf(child) : foldLeaves(operation) (empty) (isLeaf) (onLeaf) (child)))
             (empty)
             (getChildren(node))
 }
 
-const concatTree = foldTree(concat) ([])
+const concatLeaves = foldLeaves(concat) ([])
 
 module.exports = {
-    foldTree,
-    concatTree
+    foldLeaves,
+    concatLeaves
 }
