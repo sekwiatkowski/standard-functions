8339d20a66604e64108a23e2e9d6e54e19c310b1
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-04 00:07:27 +0200

    higher-order-functions: added applyMany

--- src/higher-order-functions.js
+++ src/higher-order-functions.js
@@ -18,6 +18,27 @@ function applyPairTo(f) {
     return ([a, b]) => f(a) (b)
 }
 
+function applyMany(fs) {
+    return arr => {
+        const numberOfFunctions = fs.length
+        const numberOfValues = arr.length
+
+        const res = Array(numberOfFunctions * numberOfValues)
+
+        for (let i_f = 0; i_f < numberOfFunctions; i_f++) {
+            const f = fs[i_f]
+
+            for (let i_x = 0; i_x < numberOfValues; i_x++) {
+                const x = arr[i_x]
+
+                res[i_f * numberOfValues + i_x] = f(x)
+            }
+        }
+
+        return res
+    }
+}
+
 function compose() {
     const arr = Array.prototype.slice.call(arguments)
 
@@ -47,6 +68,8 @@ module.exports = {
 
     apply,
     applyTo,
+
+    applyMany,
 
     compose,
     perform,
