0d4fcd831d52724c6b3e4867351e13c040ceacd2
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-04 02:53:11 +0200

    tree-functions: the getChildren function parameter is now fixed to be the value of an entry (if the value is an object). Both the key and the value are now available to the onLeaf function (as a pair).

--- src/tree-functions.js
+++ src/tree-functions.js
@@ -1,14 +1,25 @@
+const {isObject} = require('./object-functions')
+const {entries} = require('./object-functions')
 const {concat} = require('./string-or-array-functions')
 const {fold} = require('./array-functions')
 
 function foldLeaves(operation) {
-    return empty => getChildren => isLeaf => onLeaf => node =>
-        fold
-            ((acc, child) => operation
-                (acc)
-                (isLeaf(child) ? onLeaf(child) : foldLeaves(operation) (empty) (isLeaf) (onLeaf) (child)))
-            (empty)
-            (getChildren(node))
+    return empty => isLeaf => onLeaf => node => {
+        return fold
+        ((acc, [key, value]) => {
+            if (isLeaf(value)) {
+                return operation (acc) ([ onLeaf([key, value]) ])
+            }
+            else if (isObject(value)) {
+                return operation (acc) (foldLeaves(operation) (empty) (isLeaf) (onLeaf) (value))
+            }
+            else {
+                return acc
+            }
+        })
+        (empty)
+        (entries(node))
+    }
 }
 
 const concatLeaves = foldLeaves(concat) ([])
