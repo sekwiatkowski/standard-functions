07132bd35149de796f7888a550c383902d8038ec
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-04 04:57:40 +0200

    tree-functions: made the path available to the function applied to the leaf

--- src/tree-functions.js
+++ src/tree-functions.js
@@ -1,30 +1,26 @@
+const {append} = require('./string-or-array-functions')
 const {isObject} = require('./object-functions')
 const {entries} = require('./object-functions')
 const {concat} = require('./string-or-array-functions')
 const {fold} = require('./array-functions')
 
 function foldLeaves(operation) {
-    return empty => isLeaf => onLeaf => node => {
-        return fold
-        ((acc, [key, value]) => {
-            if (isLeaf(value)) {
-                return operation (acc) ([ onLeaf([key, value]) ])
-            }
-            else if (isObject(value)) {
-                return operation (acc) (foldLeaves(operation) (empty) (isLeaf) (onLeaf) (value))
-            }
-            else {
-                return acc
-            }
-        })
-        (empty)
-        (entries(node))
-    }
+    return empty => path => isLeaf => onLeaf => node =>
+        fold
+            ((acc, [key, value]) => {
+                if (isLeaf(value)) {
+                    return operation(acc) ([onLeaf([key, value, path])])
+                }
+                else if (isObject(value)) {
+                    const updatedPath = append(key)(path)
+                    return operation(acc) (foldLeaves(operation) (empty) (updatedPath) (isLeaf) (onLeaf) (value))
+                }
+                else {
+                    return acc
+                }
+            })
+            (empty)
+            (entries(node))
 }
 
-const concatLeaves = foldLeaves(concat) ([])
-
-module.exports = {
-    foldLeaves,
-    concatLeaves
-}
+const concatLeaves = foldLeaves(concat) ([]) ([])
