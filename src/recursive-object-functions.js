717a53f6943d1c0f3ff572436ac4eb01cdb04915
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-19 04:31:57 +0200

    recursive-object-functions: fixed mergeRecursiveProperties

--- src/recursive-object-functions.js
+++ src/recursive-object-functions.js
@@ -1,21 +1,22 @@
+const {merge} = require('./object-functions')
 const {hasProperty} = require('./object-functions')
 const {omit} = require('./object-functions')
 const {values} = require('./object-functions')
 const {flatMap} = require('./array-functions')
 
-function mergeRecursiveProperties(operation) {
-    return acc => property => {
+function mergeRecursivePropertiesStep(acc) {
+    return property => {
         const omitRecursiveProperty = omit([property])
 
         return node => {
             const withoutRecursiveProperty = omitRecursiveProperty(node)
 
-            const ownResult = operation(acc)(withoutRecursiveProperty)
+            const ownResult = merge(acc)(withoutRecursiveProperty)
 
             if (hasProperty(property)(node)) {
                 const sub = node[property]
 
-                const childResults = flatMap(value => mergeRecursiveProperties(operation)(ownResult)(property)(value))(values(sub))
+                const childResults = flatMap(value => mergeRecursivePropertiesStep(ownResult)(property)(value))(values(sub))
 
                 return [ownResult, ...childResults]
             } else {
@@ -24,6 +25,8 @@ function mergeRecursiveProperties(operation) {
         }
     }
 }
+
+const mergeRecursiveProperties = mergeRecursivePropertiesStep([])
 
 function recursiveProperty(name) {
     const omitRecursiveProperty = omit([name])
