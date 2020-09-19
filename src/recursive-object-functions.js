58a5b982a60df38899099e319ef4c516bf307e64
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-20 01:02:49 +0200

    recursive-object-functions: renamed mergeRecursiveProperties to mergeRecursively, added mergeRecursivelyWithPath

--- src/recursive-object-functions.js
+++ src/recursive-object-functions.js
@@ -1,10 +1,12 @@
+const {appendTo} = require('./string-or-array-functions')
+const {entries} = require('./object-functions')
 const {merge} = require('./object-functions')
 const {hasProperty} = require('./object-functions')
 const {omit} = require('./object-functions')
 const {values} = require('./object-functions')
 const {flatMap} = require('./array-functions')
 
-function mergeRecursivePropertiesStep(acc) {
+function mergeRecursivelyStep(acc) {
     return property => {
         const omitRecursiveProperty = omit([property])
 
@@ -16,7 +18,7 @@ function mergeRecursivePropertiesStep(acc) {
             if (hasProperty(property)(node)) {
                 const sub = node[property]
 
-                const childResults = flatMap(value => mergeRecursivePropertiesStep(ownResult)(property)(value))(values(sub))
+                const childResults = flatMap(value => mergeRecursivelyStep(ownResult)(property)(value))(values(sub))
 
                 return [ownResult, ...childResults]
             } else {
@@ -26,7 +28,31 @@ function mergeRecursivePropertiesStep(acc) {
     }
 }
 
-const mergeRecursiveProperties = mergeRecursivePropertiesStep([])
+const mergeRecursively = mergeRecursivelyStep([])([])
+
+function mergeRecursivelyWithPathStep(acc) {
+    return path => property => {
+        const omitRecursiveProperty = omit([property])
+
+        return node => {
+            const withoutRecursiveProperty = omitRecursiveProperty(node)
+
+            const ownResult = [path, merge(acc)(withoutRecursiveProperty)]
+
+            if (hasProperty(property)(node)) {
+                const sub = node[property]
+
+                const childResults = flatMap(([key, value]) => mergeRecursivelyStep(ownResult)(appendTo(path)(key))(property)(value))(entries(sub))
+
+                return [ownResult, ...childResults]
+            } else {
+                return [ownResult]
+            }
+        }
+    }
+}
+
+const mergeRecursivelyWithPath = mergeRecursivelyWithPathStep([])([])
 
 function recursiveProperty(name) {
     const omitRecursiveProperty = omit([name])
@@ -49,5 +75,6 @@ function recursiveProperty(name) {
 
 module.exports = {
     recursiveProperty,
-    mergeRecursiveProperties
+    mergeRecursively,
+    mergeRecursivelyWithPath
 }
