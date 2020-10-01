839ab6619993e86dcd2c802e38b5f69d42bfa828
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-01 21:13:03 +0200

    higher-order-functions: removed applyMany

--- src/higher-order-functions.js
+++ src/higher-order-functions.js
@@ -18,27 +18,6 @@ export function applyPairTo(f) {
     return ([a, b]) => f(a) (b)
 }
 
-export function applyMany(fs) {
-    return arr => {
-        const numberOfFunctions = fs.length
-        const numberOfValues = arr.length
-
-        const res = Array(numberOfFunctions * numberOfValues)
-
-        for (let i_f = 0; i_f < numberOfFunctions; i_f++) {
-            const f = fs[i_f]
-
-            for (let i_x = 0; i_x < numberOfValues; i_x++) {
-                const x = arr[i_x]
-
-                res[i_f * numberOfValues + i_x] = f(x)
-            }
-        }
-
-        return res
-    }
-}
-
 export function compose() {
     const arr = Array.prototype.slice.call(arguments)
 
