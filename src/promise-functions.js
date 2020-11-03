1a18af78b93b2e97731bfe5fd831ab886e6caf51
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-11-03 23:32:12 +0100

    promise-functions: implemented a then function to map resolved promises either to a value or using a function

--- src/promise-functions.js
+++ src/promise-functions.js
@@ -1,7 +1,12 @@
 import {foldResult} from './result'
+import {isFunction} from './higher-order-functions'
 
 export function reject(reason) {
     return Promise.reject(reason)
+}
+
+export function then(functionOrValue) {
+    return promise => promise.then(isFunction(functionOrValue) ? functionOrValue : () => functionOrValue)
 }
 
 export function transformResultToPromise(f) {
