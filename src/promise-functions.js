cd8ebd485a03acd1a2be47651893a642c846be44
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-11-04 17:08:38 +0100

    promise-functions: added resolved, renamed then to mapFulfilled and added mapPromised

--- src/promise-functions.js
+++ src/promise-functions.js
@@ -1,12 +1,22 @@
 import {foldResult} from './result'
 import {isFunction} from './higher-order-functions'
 
+export function resolve(value) {
+    return Promise.resolve(value)
+}
+
 export function reject(reason) {
     return Promise.reject(reason)
 }
 
-export function then(functionOrValue) {
+export function mapFulfilled(functionOrValue) {
     return promise => promise.then(isFunction(functionOrValue) ? functionOrValue : () => functionOrValue)
+}
+
+export function mapPromise(ifFulfilled) {
+    return ifRejected => promise => promise.then(
+        isFunction(ifFulfilled) ? ifFulfilled : () => ifFulfilled,
+        isFunction(ifRejected) ? ifRejected : () => ifRejected)
 }
 
 export function transformResultToPromise(f) {
