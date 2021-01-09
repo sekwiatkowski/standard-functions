4f519be6d32bda9fc8326377593f3c617ff96d29
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-01-09 23:47:02 +0100

    date-functions: added functions to add time units to the current date

--- src/date-functions.js
+++ src/date-functions.js
@@ -89,3 +89,23 @@ export function addHours(hours) {
 export function addDays(days) {
     return addHours(daysToHours(days))
 }
+
+export function inMilliseconds(milliseconds) {
+    return addMilliseconds(milliseconds) (new Date())
+}
+
+export function inSeconds(seconds) {
+    return inMilliseconds(secondsToMilliseconds(seconds))
+}
+
+export function inMinutes(minutes) {
+    return inSeconds(minutesToSeconds(minutes))
+}
+
+export function inHours(hours) {
+    return inMinutes(hoursToMinutes(hours))
+}
+
+export function inDays(days) {
+    return inHours(daysToHours(days))
+}
