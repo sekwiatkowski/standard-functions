070021f129ed4c1c9d83172a3003a529a0c1a2aa
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-11-16 01:41:18 +0100

    character-functions: exported alphabets

--- dist/character-functions.js
+++ dist/character-functions.js
@@ -6,19 +6,23 @@ Object.defineProperty(exports, "__esModule", {
 exports.isDigit = isDigit;
 exports.isLowercaseEnglishLetter = isLowercaseEnglishLetter;
 exports.isUppercaseEnglishLetter = isUppercaseEnglishLetter;
+exports.uppercaseEnglishLetters = exports.lowercaseEnglishLetters = exports.digits = void 0;
 var digits = '0123456789';
+exports.digits = digits;
 
 function isDigit(c) {
   return digits.includes(c);
 }
 
 var lowercaseEnglishLetters = 'abcdefghijklmnopqrstuvwxyz';
+exports.lowercaseEnglishLetters = lowercaseEnglishLetters;
 
 function isLowercaseEnglishLetter(c) {
   return lowercaseEnglishLetters.includes(c);
 }
 
 var uppercaseEnglishLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+exports.uppercaseEnglishLetters = uppercaseEnglishLetters;
 
 function isUppercaseEnglishLetter(c) {
   return uppercaseEnglishLetters.includes(c);
--- src/character-functions.js
+++ src/character-functions.js
@@ -1,16 +1,16 @@
-const digits = '0123456789'
+export const digits = '0123456789'
 
 export function isDigit(c) {
     return digits.includes(c)
 }
 
-const lowercaseEnglishLetters = 'abcdefghijklmnopqrstuvwxyz'
+export const lowercaseEnglishLetters = 'abcdefghijklmnopqrstuvwxyz'
 
 export function isLowercaseEnglishLetter(c) {
     return lowercaseEnglishLetters.includes(c)
 }
 
-const uppercaseEnglishLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+export const uppercaseEnglishLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 export function isUppercaseEnglishLetter(c) {
     return uppercaseEnglishLetters.includes(c)
